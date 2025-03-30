0fddb320a78457bec252133fb6e499027e9b2541
Thushara Navod  Padiwela <146652282+thusharanavod123@users.noreply.github.com>
2025-03-30 23:27:29 +0530

    app.jsx

---
+++ App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Main } from './components/Main/Main'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    
+        <>
+
+        <sidebar>
+          <Main />
+        </sidebar>
+
+
+        </>
+
+
+
+  )
+}
+
+export default App
